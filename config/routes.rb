40f5611222fb2a122a876045aadb0c5d0829ee6a
luke1069 <luke1069onega14mas@gmail.com>
2021-12-05 23:33:39 +0000

    googlemap_api導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # googlemap_api関連
+  resources :maps, only: [:index]
+  get '/map_request', to: 'maps#map', as: 'map_request'
+
+  devise_for :users
+  root to: 'homes#top'
 end
