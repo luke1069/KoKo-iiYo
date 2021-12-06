bfa36d11caea48bc004f98a1eca700d9a46dc4e9
luke1069 <luke1069onega14mas@gmail.com>
2021-12-06 00:35:21 +0000

    googlemap_api導入

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -3,13 +3,13 @@ class MapsController < ApplicationController
   def index
   end
 
-  def map
-    results = Geocoder.search(params[:address])
-    @latlng = results.first.coordinates
+  # def map
+  #   results = Geocoder.search(params[:address])
+  #   @latlng = results.first.coordinates
 
-    respond_to do |format|
-      format.js
-    end
-  end
+  #   respond_to do |format|
+  #     format.js
+  #   end
+  # end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   # googlemap_api関連
   resources :maps, only: [:index]
-  get '/map_request', to: 'maps#map', as: 'map_request'
+  # get '/map_request', to: 'maps#map', as: 'map_request'
 
   devise_for :users
   root to: 'homes#top'
