d9105108c8e7fa4e2004079f3c218cf045989fe5
luke1069 <luke1069onega14mas@gmail.com>
2021-12-08 13:08:29 +0000

    DM機能実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,28 @@
 class MessagesController < ApplicationController
+
+  before_action :authenticate_user!, only: [:create, :destroy]
+
+  def create
+    if Entry.where(user_id: current_user.id, room_id: params[:message][:room_id]).present?
+      @message = Message.new(message_params)
+      if @message.save
+        redirect_to room_path(@message.room_id)
+      else
+        redirect_to room_path(@message.room_id)
+      end
+    end
+  end
+
+  def destroy
+    @message = Message.find(:id)
+    @message.destroy
+    redirect_to room_path(@message.room_id)
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:user_id, :room_id, :message).merge(user_id: current_user.id)
+  end
+
 end
--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,2 +1,29 @@
 class RoomsController < ApplicationController
+
+  before_action :authenticate_user!
+
+  def create
+    @room = Room.create
+    @joinCurrentUser = Entry.create(user_id: current_user.id, room_id: @room.id)
+    @joinUser = Entry.create(join_room_params)
+    redirect_to room_path(@room)
+  end
+
+  def show
+    @room = Room.find(params[:id])
+    if Entry.where(user_id: current_user.id, room_id: @room.id).present?
+      @messages = @room.messages
+      @message = Message.new
+      @entries = @room.entries
+    else
+      redirect_to request.referer
+    end
+  end
+
+  private
+
+  def join_room_params
+    params.require(:entry).permit(:user_id, :room_id).merge(room_id: @room.id)
+  end
+
 end
