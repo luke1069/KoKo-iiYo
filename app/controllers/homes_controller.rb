681a73effc3b8afc6455c33962d7330578efd237
luke1069 <luke1069onega14mas@gmail.com>
2021-12-10 02:39:18 +0000

    新着・人気投稿一覧機能

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,12 @@
 class HomesController < ApplicationController
 
 def top
-  @posts = Post.all
+  @posts_new = Post.all.order(id: "DESC")
+  @posts_favorite = Post.includes(:favorites).
+    sort { |a,b|
+      b.favorites.count <=>
+      a.favorites.count
+    }
 end
 
 end
