fe6a8cd54a74ec8145949969b43d78214bb967c3
luke1069 <luke1069onega14mas@gmail.com>
2021-12-15 05:48:59 +0000

    topページ編集

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,6 +2,7 @@
 //= require popper
 //= require bootstrap-sprockets
 
+//= require jquery
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
@@ -17,6 +18,40 @@
  * version: 3.1.1
  *
  */
+
+$(document).ready(function () {
+  $("#images").skippr({
+    // スライドショーの変化 ("fade" or "slide")
+    transition : 'slide',
+    // 変化に係る時間(ミリ秒)
+    speed : 2000,
+    // easingの種類
+    easing : 'easeOutQuart',
+    // ナビゲーションの形("block" or "bubble")
+    navType : 'block',
+    // 子要素の種類("div" or "img")
+    childrenElementType : 'div',
+    // ナビゲーション矢印の表示(trueで表示)
+    arrows : true,
+    // スライドショーの自動再生(falseで自動再生なし)
+    autoPlay : true,
+    // 自動再生時のスライド切替間隔(ミリ秒)
+    autoPlayDuration : 5000,
+    // キーボードの矢印キーによるスライド送りの設定(trueで有効)
+    keyboardOnAlways : true,
+    // 一枚目のスライド表示時に戻る矢印を表示するかどうか(falseで非表示)
+    hidePrevious : false
+  });
+});
+
+$(function() {
+  $('#back a').on('click',function(event){
+    $('body, html').animate({
+      scrollTop:0
+    }, 800);
+    event.preventDefault();
+  });
+});
 
 (function(root, factory) {
   if (typeof define === 'function' && define.amd) {
