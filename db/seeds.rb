8f81c6d75ef80f823c48e42ce3cc253de3404de6
luke1069 <luke1069onega14mas@gmail.com>
2021-12-22 06:18:33 +0000

    seedファイル

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(
+  [
+    {
+      name: 'test1',
+      email: 'test1@test.com',
+      encrypted_password: 'luke0630',
+      introduction: '宜しくお願い致します。',
+    },
+    {
+      name: 'test2',
+      email: 'test2@test.com',
+      encrypted_password: 'luke0630',
+      introduction: '宜しくお願い致します。'
+    },
+    {
+      name: 'test3',
+      email: 'test3@test.com',
+      encrypted_password: 'luke0630',
+      introduction: '宜しくお願い致します。'
+    },
+    {
+      name: 'test4',
+      email: 'test4@test.com',
+      encrypted_password: 'luke0630',
+      introduction: '宜しくお願い致します。'
+    },
+    {
+      name: 'test5',
+      email: 'test5@test.com',
+      encrypted_password: 'luke0630',
+      introduction: '宜しくお願い致します。'
+    }
+    ]
+  )
