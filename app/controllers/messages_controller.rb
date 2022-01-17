375bd27d9d93c60b3773d6eee7ecc091d03c5933
luke1069 <luke1069onega14mas@gmail.com>
2022-01-17 15:47:45 +0000

    ajax非同期通信

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,19 +5,21 @@ class MessagesController < ApplicationController
   def create
     if Entry.where(user_id: current_user.id, room_id: params[:message][:room_id]).present?
       @message = Message.new(message_params)
-      if @message.save
-        redirect_to room_path(@message.room_id)
-      else
-        redirect_to room_path(@message.room_id)
-      end
+      @message.save
+      @messages = Message.where(room_id: @message.room_id)
+      #   redirect_to room_path(@message.room_id)
+      # else
+      #   redirect_to room_path(@message.room_id)
+      # end
     end
   end
 
   def destroy
     @message = Message.find(params[:id])
     if @message.user_id == current_user.id
+      @messages = Message.where(room_id: @message.room_id)
       @message.destroy
-      redirect_to room_path(@message.room_id)
+      # redirect_to room_path(@message.room_id)
     end
   end
 
