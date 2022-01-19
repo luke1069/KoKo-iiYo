925a824553732d66e534daa0c1e0ca8f7ca09481
luke1069 <luke1069onega14mas@gmail.com>
2022-01-19 07:18:39 +0000

    relationships view修正

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -15,10 +15,52 @@ class RelationshipsController < ApplicationController
   def following
     @user = User.find(params[:user_id])
     @users = @user.followings
+    @users = Kaminari.paginate_array(@users).page(params[:page]).per(10)
+    # チャット機能(条件分岐)
+    # @users.each_with_index do |following_u, i|
+    #   @user_i = following_u
+    #   @currentUserEntry = Entry.where(user_id: current_user.id)
+    #   @userEntry = Entry.where(user_id: @user_i.id)
+    #   unless @user_i.id == current_user.id
+    #     @currentUserEntry.each do |cu|
+    #       @userEntry.each do |u|
+    #         if cu.room_id == u.room_id
+    #           @haveRoom = true
+    #           @roomId = cu.room_id
+    #         end
+    #       end
+    #     end
+    #   end
+    #   unless @haveRoom == true
+    #     @room = Room.new
+    #     @entry = Entry.new
+    #   end
+    # end
   end
 
   def followed
     @user = User.find(params[:user_id])
     @users = @user.followers
+    @users = Kaminari.paginate_array(@users).page(params[:page]).per(10)
+    # チャット機能(条件分岐)
+    # @users.each_with_index do |followed_u, i|
+    #   @user_i = followed_u
+    #   @currentUserEntry = Entry.where(user_id: current_user.id)
+    #   @userEntry = Entry.where(user_id: @user_i.id)
+    #   unless @user_i.id == current_user.id
+    #     @currentUserEntry.each do |cu|
+    #       @userEntry.each do |u|
+    #         if cu.room_id == u.room_id
+    #           @haveRoom = true
+    #           @roomId = cu.room_id
+    #         end
+    #       end
+    #     end
+    #   end
+    #   unless @haveRoom == true
+    #     @room = Room.new
+    #     @entry = Entry.new
+    #   end
+    # end
   end
 end
